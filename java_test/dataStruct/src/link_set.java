fac9a9e97421cb358f44527bd810c5581ef77472
juli0213 <1132053315@qq.com>
2021-07-24 11:19:40 +0800

    datastruct

---
+++ java_test/dataStruct/src/link_set.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+public class link_set {
+    public static void main(String[] a) {
+        LinkedList officers = new LinkedList();
+        officers.add("B");
+        officers.add("B");
+        officers.add("T");
+        officers.add("H");
+        officers.add("P");
+        System.out.println(officers);
+        officers.set(2, "M");
+        System.out.println(officers);
+    }
+}
